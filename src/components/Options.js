131dbe579c1c957fce9fd369ebcbc25379bbd7d4
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-12-25 22:45:04 -0700

    finished the first page

---
+++ src/components/Options.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Options extends Component {
+    render() {
+        let cat = ''
+        cat = this.props.categories.length > 1 ? 
+        this.props.categories.map((s, i) =>
+            <option
+             key = {i}
+             value = {s.id}
+             > {s.name}</option>
+
+        ): null
+        return (
+            <div className='input-group mb-3 divOps'>
+             <select
+              className='custom-select'
+              onChange={(e)=> this.props.supChanged(e.target.value, this.props.name)} 
+             >{cat}</select>
+             <div className="input-group-append divLabel">
+          <label className="input-group-text" >
+            {this.props.name.charAt(0).toUpperCase() + this.props.name.slice(1)}</label>
+        </div>
+                
+            </div>
+        )
+    }
+}
--- src/container/Welcome.js
+++ src/container/Welcome.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import Header from '../components/Header'
+import Options from '../components/Options'
 
 
 export default class Welcome extends Component {
@@ -15,18 +16,32 @@ export default class Welcome extends Component {
             <Fragment>
                 <div className='container'>
                 <Header/>
-                <div className='selectOptions'>
-                <h1>Select game options</h1>
-                
+                    <div className='selectOptions'>
+                        <h1>Select game options</h1>
+                        <Options
+                        name={'category'}
+                        categories ={this.props.categories}
+                        selected = {this.props.selectedCat}
+                        supChanged = {(el,name) =>{
+                            this.props.choosing(el,name)
+                        }}
+                        />
 
-                
-                
-                <button 
-                onClick={()=>this.click()} 
-                type='button' 
-                className='btn btn-primary'
-                >Play Game</button>
-                </div>
+                        <Options
+                        name={'level'}
+                        categories = {this.props.level}
+                        selected={this.props.selectedLev}
+                        supChanged = {(el,name) =>{
+                            this.props.choosing(el,name)
+                        }}
+                        />
+                        
+                        <button 
+                        onClick={()=>this.click()} 
+                        type='button' 
+                        className='btn btn-primary'
+                        >Play Game</button>
+                    </div>
                 </div>
              </Fragment>
         )
