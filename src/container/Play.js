e6359bb58b91add920ce087e3b360c69b152f112
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-12-29 23:58:38 -0700

    commit

--- src/container/Play.js
+++ src/container/Play.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 export default class Play extends Component {
+    state={
+        questions: {},
+        time: 15
+    }
+
+    componentDidMount(){
+        this.setState({
+            questions: this.props.questions
+        })
+        this.timer()
+    }
+    timer(){
+        setinterval(()=>{
+        if (this.state.time <= 0){
+            this.setState
+
+        }
+        })
+    }
     render() {
         return (
             <div>
-                <h1>{this.props.questions}</h1>
+                <h1></h1>
             </div>
         )
     }
